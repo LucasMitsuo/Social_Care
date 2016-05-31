2b4bf62598842a954c0b21820f53c68a916752f3
Lucas Mitsuo <lucas_mitsuo011@hotmail.com>
2016-05-31 01:45:35 -0300

    Implementação da lista de procedimentos

--- SocialCare/SocialCare/Controllers/ServicosController.cs
+++ SocialCare/SocialCare/Controllers/ServicosController.cs
@@ -9,19 +9,31 @@ using System.Web.Http;
 
 namespace SocialCare.Controllers
 {
+
     public class ServicosController : ApiController
     {
+        SocialCareEntities db = new SocialCareEntities();
+
         [HttpGet]
         [Route("api/cid10")]
         public HttpResponseMessage ObterCID10()
         {
-            SocialCareEntities db = new SocialCareEntities();
-
             var result = db.TAB_CID.ToList();
 
             var colCID10 = from c in result select new CID10Dto(c).descricao;
 
             return Request.CreateResponse(HttpStatusCode.OK, colCID10.ToArray());
+        }
+
+        [HttpGet]
+        [Route("api/procedimentos")]
+        public HttpResponseMessage ObterProcedimentos()
+        {
+            var result = db.TAB_PROCEDIMENTO.ToList();
+
+            var colProcedimentos = from p in result select new ProcedimentoDto(p).descricao;
+
+            return Request.CreateResponse(HttpStatusCode.OK, colProcedimentos.ToArray());
         }
     }
 }
---
+++ SocialCare/SocialCare/DTO/ProcedimentoDto.cs
@@ -0,0 +1,20 @@
+﻿using SocialCare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialCare.DTO
+{
+    public class ProcedimentoDto
+    {
+        public int id;
+        public string descricao;
+
+        public ProcedimentoDto(TAB_PROCEDIMENTO procedimento)
+        {
+            this.id = procedimento.cod_procedimento;
+            this.descricao = procedimento.nom_procedimento + " - " + procedimento.obs_procedimento;
+        }
+    }
+}
