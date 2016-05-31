1f7dc356fa7f40d689a0f16c2eab4dede3aed00e
Lucas Mitsuo <lucas_mitsuo011@hotmail.com>
2016-05-31 00:01:30 -0300

    Criação do projeto

---
+++ SocialCare/SocialCare/Controllers/ServicosController.cs
@@ -0,0 +1,27 @@
+﻿using SocialCare.DTO;
+using SocialCare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SocialCare.Controllers
+{
+    public class ServicosController : ApiController
+    {
+        [HttpGet]
+        [Route("api/cid10")]
+        public HttpResponseMessage ObterCID10()
+        {
+            SocialCareEntities db = new SocialCareEntities();
+
+            var result = db.TAB_CID.ToList();
+
+            var colCID10 = from c in result select new CID10Dto(c).descricao;
+
+            return Request.CreateResponse(HttpStatusCode.OK, colCID10.ToArray());
+        }
+    }
+}
---
+++ SocialCare/SocialCare/DTO/CID10Dto.cs
@@ -0,0 +1,21 @@
+﻿using SocialCare.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialCare.DTO
+{
+    public class CID10Dto
+    {
+        public int id { get; set; }
+        public string descricao { get; set; }
+
+
+        public CID10Dto(TAB_CID cid10)
+        {
+            this.id = cid10.cod_cid;
+            this.descricao = cid10.cod_cid10 + " - " + cid10.des_cid;
+        }
+    }
+}
