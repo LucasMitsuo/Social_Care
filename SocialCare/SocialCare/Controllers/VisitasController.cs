0f2bf3df6b791164197f9270310b2126a72b58e3
Lucas Mitsuo <lucas_mitsuo011@hotmail.com>
2016-06-01 01:51:26 -0300

    Criação dos controladores de visita e pacientes

---
+++ SocialCare/SocialCare/Controllers/VisitasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SocialCare.Controllers
+{
+    public class VisitasController : Controller
+    {
+        // GET: Visitas
+        public ActionResult Lista()
+        {
+            return View();
+        }
+    }
+}
